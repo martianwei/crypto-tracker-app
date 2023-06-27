b6bb8e73e63d98a4aa947e5e9415950603df874f
martianwei <martianwei456@gmail.com>
2023-06-27 15:17:28 +0800

    add timeline

--- src/components/TrackView.vue
+++ src/components/TrackView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="track-view">
-    <svg class="box" :style="{ height: boxHeight + 'px' }" v-if="jsonData">
+    <svg class="box" :style="{ height: boxHeight + 'px', width: boxWidth + 'px' }" v-if="jsonData">
       <!-- 添加时间轴箭头定义 -->
       <defs>
         <marker id="arrowhead" viewBox="0 0 10 10" refX="5" refY="5" markerWidth="8" markerHeight="8"
@@ -10,10 +10,15 @@
       </defs>
       <!-- 添加时间轴 -->
       <line class="timeline" :x1="500" :y1="30" :x2="1800" :y2="30" />
+      <!-- 添加时间轴上的刻度 -->
+      <g v-for="index in 10" :key="index">
+        <!-- <text class="timeline-text" :x="500 + index * 130" :y="20">{{ minTimestamp + index * 86400 }}</text> -->
+        <line class="timeline-item" :x1="500 + index * 30" :y1="15" :x2="500 + index * 30" :y2="45" />
+      </g>
       <!-- 其他元素保持不变 -->
       <g v-for="(address, index) in addresses" :key="index">
         <text class="address-text" :x="0" :y="50 + index * 200">{{ address }}</text>
-        <line class="timeline-item" :x1="500" :y1="50 + index * 200" :x2="1800" :y2="50 + index * 200" />
+        <line class="address-timeline" :x1="500" :y1="50 + index * 200" :x2="1800" :y2="50 + index * 200" />
       </g>
       <g v-for="(txn) in transactions" :key="txn.id">
         <text class="txn-text" :x="500 + calculateX(txn.timestamp)" :y="50 + (txn.sender + txn.receiver - 2) * 100">
@@ -38,12 +43,22 @@ export default {
       addresses: [],
       transactions: [],
       minTimestamp: Number.MAX_VALUE, // 用于记录最小的时间戳
+      maxTimestamp: Number.MIN_VALUE, // 用于记录最大的时间戳
     };
   },
   computed: {
     boxHeight() {
       return this.addresses.length * 200; // 根据地址数量计算高度，每个地址占据200px，加上额外的100px
     },
+    boxWidth() {
+      // 根据最小和最大时间戳计算交易时间范围并转换为小时数
+      const rangeHours = (this.maxTimestamp - this.minTimestamp) / 3600;
+      // 根据每小时像素宽度计算 boxWidth
+      const hourlyWidth = 40; // 每小时的像素宽度，根据需要调整
+      const boxWidth = rangeHours * hourlyWidth + 1000;
+
+      return boxWidth;
+    }
   },
   async created() {
     await this.fetchData();
@@ -72,6 +87,9 @@ export default {
         this.jsonData.data.inbound.forEach((item) => {
           const senderAddress = item.sender.address;
           const receiverAddress = item.receiver.address;
+          const timestamp = item.timestamp;
+          this.minTimestamp = Math.min(this.minTimestamp, timestamp); // 更新最小的时间戳
+          this.maxTimestamp = Math.max(this.maxTimestamp, timestamp); // 更新最大的时间戳
 
           // 添加地址到 Map，并为每个地址分配一个唯一的 ID
           if (!addressesMap.has(senderAddress)) {
@@ -82,9 +100,6 @@ export default {
             const receiverId = addressesMap.size + 1; // 分配 ID
             addressesMap.set(receiverAddress, receiverId);
           }
-
-          const timestamp = item.timestamp;
-          this.minTimestamp = Math.min(this.minTimestamp, timestamp); // 更新最小的时间戳
 
           const transaction = {
             date: this.formatTimestamp(item.timestamp),
@@ -100,6 +115,9 @@ export default {
         this.jsonData.data.outbound.forEach((item) => {
           const senderAddress = item.sender.address;
           const receiverAddress = item.receiver.address;
+          const timestamp = item.timestamp;
+          this.minTimestamp = Math.min(this.minTimestamp, timestamp); // 更新最小的时间戳
+          this.maxTimestamp = Math.max(this.maxTimestamp, timestamp); // 更新最大的时间戳
 
           // 添加地址到 Map，并为每个地址分配一个唯一的 ID
           if (!addressesMap.has(senderAddress)) {
@@ -129,7 +147,6 @@ export default {
     calculateX(timestamp) {
       const hours = (timestamp - this.minTimestamp) / 3600; // 将时间差转换为小时数
       const x = hours * 40; // 根据实际需求计算 x 坐标值的偏移量
-      console.log('x', x);
       return x;
     }
   }
@@ -140,7 +157,6 @@ export default {
 /* Set the size of the SVG container */
 .track-view {
   display: flex;
-  width: 100%;
   height: auto;
   justify-content: center;
   align-items: center;
@@ -150,7 +166,6 @@ export default {
 .box {
   display: flex;
   flex-direction: column;
-  width: 100%;
   outline: auto;
   padding: 10px;
   box-sizing: border-box;
@@ -171,9 +186,14 @@ export default {
   stroke-width: 2;
 }
 
-.timeline-item {
+.address-timeline {
   stroke: #ff0000;
   stroke-width: 5px;
+}
+
+.timeline-item {
+  stroke: #00ff95;
+  stroke-width: 1;
 }
 
 .txn-line {
